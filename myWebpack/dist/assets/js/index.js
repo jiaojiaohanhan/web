42f6221342d9018dffb18a63469a0d40fab08e11
jiaojiaohanhan <870303709@qq.com>
2018-01-29 22:49:06 +0800

    shili

---
+++ myWebpack/dist/assets/js/index.js
@@ -0,0 +1,37 @@
+/*! JH */
+webpackJsonp([0],{
+
+/***/ 2:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+var _index = __webpack_require__(1);
+
+var _index2 = _interopRequireDefault(_index);
+
+var _jquery = __webpack_require__(0);
+
+var _jquery2 = _interopRequireDefault(_jquery);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+// import sass from './css/common.scss';
+// document.write('1789');
+// let name = 'zs';
+// let a = 'school';
+// let obj = {
+//     name,
+//     age:10,
+//     a:"weichuang"
+// }
+// document.write(obj.a);
+// let add = (x,y) =>{
+//     return x+y;
+// };
+(0, _jquery2.default)(".aa").css("background", "red");
+
+/***/ })
+
+},[2]);
