933d9f89cea14af15667d02e6e430273631ae8ea
jiaojiaohanhan <870303709@qq.com>
2019-03-25 22:51:15 +0800

    shili

--- 小米官网/js/lunbo.js
+++ 小米官网/js/lunbo.js
@@ -8,11 +8,28 @@ var oBtn = document.getElementById("btns");
 var aLi = oBtn.getElementsByTagName("li");
 var oSp1 = document.getElementById("prev-btn");
 var oSp2 = document.getElementById("next-btn");
+
+var oBook = document.getElementById("book");
+var oBoDiv1 = oBook.getElementsByTagName("div")[0];
+var oBoDiv2 = oBook.getElementsByTagName("div")[1];
+var aBo1 = oBoDiv1.getElementsByTagName("li");
+var aBo2 = oBoDiv2.getElementsByTagName("li");
 var oMiui = document.getElementById("miui");
 var oMiDiv1 = oMiui.getElementsByTagName("div")[0];
 var oMiDiv2 = oMiui.getElementsByTagName("div")[1];
 var aMi1 = oMiDiv1.getElementsByTagName("li");
 var aMi2 = oMiDiv2.getElementsByTagName("li");
+var oGame = document.getElementById("game");
+var oGaDiv1 = oGame.getElementsByTagName("div")[0];
+var oGaDiv2 = oGame.getElementsByTagName("div")[1];
+var aGa1 = oGaDiv1.getElementsByTagName("li");
+var aGa2 = oGaDiv2.getElementsByTagName("li");
+var oApp = document.getElementById("app");
+var oApDiv1 = oApp.getElementsByTagName("div")[0];
+var oApDiv2 = oApp.getElementsByTagName("div")[1];
+var aAp1 = oApDiv1.getElementsByTagName("li");
+var aAp2 = oApDiv2.getElementsByTagName("li");
+
 var iNow = 0;
 var timer;
 for (var i=0;i<aLi.length;i++) {
@@ -53,15 +70,49 @@ oSp2.onclick = function () {
     aLi[iNow].className = "selected";
     aImg[iNow].className = "selected";
 }
+
+for (var i=0;i<aBo2.length;i++){
+    aBo2[i].index = i;
+    aBo2[i].onmouseover = function () {
+        for (var j=0; j<aBo2.length;j++) {
+            aBo2[j].className ="";
+            aBo1[j].className ="con-carousel-no-active";
+        }
+        this.className = "btn-active";
+        aBo1[this.index].className = "con-carousel-active";
+    }
+}
 for (var i=0;i<aMi2.length;i++){
     aMi2[i].index = i;
     aMi2[i].onmouseover = function () {
         for (var j=0; j<aMi2.length;j++) {
             aMi2[j].className ="";
-            aMi1[j].className ="";
+            aMi1[j].className ="con-carousel-no-active";
         }
         this.className = "btn-active";
         aMi1[this.index].className = "con-carousel-active";
+    }
+}
+for (var i=0;i<aGa2.length;i++){
+    aGa2[i].index = i;
+    aGa2[i].onmouseover = function () {
+        for (var j=0; j<aGa2.length;j++) {
+            aGa2[j].className ="";
+            aGa1[j].className ="con-carousel-no-active";
+        }
+        this.className = "btn-active";
+        aGa1[this.index].className = "con-carousel-active";
+    }
+}
+for (var i=0;i<aAp2.length;i++){
+    aAp2[i].index = i;
+    aAp2[i].onmouseover = function () {
+        for (var j=0; j<aAp2.length;j++) {
+            aAp2[j].className ="";
+            aAp1[j].className ="con-carousel-no-active";
+        }
+        this.className = "btn-active";
+        aAp1[this.index].className = "con-carousel-active";
     }
 }
 
